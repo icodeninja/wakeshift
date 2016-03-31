a6de8f1e107303145726dff26977a64cf47386b0
Ryan Willis <icodeninja@gmail.com>
2016-03-31 01:24:52 -0700

    socket addition

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+var MainController = angular.module('MainController',[]);
+MainController.controller('MainController',function($scope){
+  console.log('test');
+  $scope.sock = io.connect('http://' + window.location.hostname);
+  $scope.sock.on('connect', function(data){
+    console.log('inital connect');
+    $scope.sock.emit('message',{hey:'man'});
+    $scope.sock.on('message',function(data){
+      console.log(data);
+    });
+  });
+  $scope.hiThere = 'hey';
+});
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var express = require('express');
-
+var exec = require('child_process').exec;
 var app = express();
-
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var port = 80;
 
 app.use(express.static(__dirname + '/dist'));
+app.use('/modules',express.static(__dirname + '/node_modules'))
 
-app.listen(port,function(){
+app.get('/api/',function(req,res){
+  exec('ping google.com',function(err,cout,cerr){
+    var splits = "\n";
+    if(cout.indexOf("\r\n") !== -1) splits = "\r\n";
+
+    res.end(JSON.stringify(cout.split(splits)));
+  });
+});
+
+server.listen(port,function(){
   console.log("server started!");
+});
+
+io.on('connection',function(sock){
+  sock.on('message',function(json,from){
+    console.log('got me some ' + JSON.stringify(json) + ' from ' + from);
+    sock.emit('message',{'hey':'mannnnn'});
+  })
 });
