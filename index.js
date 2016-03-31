90b5c7e3402da637b4b0dd6b33a5c02394e4d466
Ryan Willis <icodeninja@gmail.com>
2016-03-31 07:44:52 +0000

    open interfaces

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ var express = require('express');
 var app = express();
 
 var port = 80;
-var host = '127.0.0.1';
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(port,host,function(){
+app.listen(port,function(){
   console.log("server started!");
 });
